5469c248593ea43a98b06aa9ff4cacf3cc46543e
cmiran0 <fpcr3524>
2019-12-13 17:55:43 +0100

    first commit

---
+++ src/main/java/bbdd/Application.java
@@ -0,0 +1,109 @@
+package bbdd;
+
+import java.sql.*;
+import java.util.Properties;
+
+public class Application {
+
+    public static void main(String[] args) {
+        try {
+            Connection connection = getConnection();
+            showClient(connection);
+            showClientStartWith(connection, "J");
+            addClient(connection, "Christian", "Miranda Balladares", "24545349W");
+            showClient(connection);
+            updateClient(connection, 4, "Maria Isabel");
+            showClient(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        Connection conn = null;
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", "admin");
+        connectionProps.put("password", "S1Sjrl2c1!");
+        long before = System.currentTimeMillis();
+        conn = DriverManager.getConnection("jdbc:mysql://dam-m06.cdy301tthrim.us-east-1.rds.amazonaws.com/prova", connectionProps);
+        long after = System.currentTimeMillis();
+        System.out.println("Connected to database " + (after - before) + " ms");
+        return conn;
+    }
+
+    public static void addClient(Connection connection, String nom, String cognoms, String dni) {
+        String query = "INSERT INTO client (nom, cognmos, dni) VALUES ('" + nom + "','" + cognoms + "','" + dni + "')";
+        Statement stmt = null;
+        try {
+            stmt = connection.createStatement();
+            stmt.executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void updateClient(Connection connection, int id, String newName) {
+        String query = "UPDATE client SET nom='" + newName + "' WHERE id=" + id;
+        insertUpdateClient(connection, query);
+    }
+
+    public static void insertUpdateClient(Connection connection, String query) {
+        System.out.println("query: " + query);
+        Statement stmt = null;
+        try {
+            stmt = connection.createStatement();
+            stmt.executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void showClient(Connection connection) throws SQLException {
+        String query = "SELECT id, nom, cognmos, dni FROM client";
+        showQueryClient(connection, query);
+    }
+
+    public static void showClientStartWith(Connection connection, String startWith) throws SQLException {
+        String query = "SELECT id, nom, cognmos, dni FROM client WHERE nom LIKE '" + startWith + "%'";
+        showQueryClient(connection, query);
+    }
+
+
+    public static void showQueryClient(Connection con, String query) throws SQLException {
+        Statement stmt = null;
+
+        try {
+            stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String nom = rs.getString("nom");
+                String cognoms = rs.getString("cognmos");
+                String dni = rs.getString("dni");
+                System.out.println("Client:\t" + id + "\tnom:\t" + nom + "\tcognom\t" + cognoms + "\ndni:\t" + dni);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+        }
+    }
+}
